5e7d324678ea9a07e5d02f2a7f01101ff776c297
nihasaru943-art <nihasaru943@gmail.com>
2026-09-08 23:40:49 +0530

    Time: 0 ms (100%), Space: 28.9 MB (78.57%) - LeetHub

---
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public int CountCommas(int n) {
+       // 100,000
+        
+        //1000
+        //1001
+        //1002
+        //n - 998
+
+        if(n < 1000)
+        {
+            return 0;
+        }
+            return n - 999;
+        
+    }
+}
